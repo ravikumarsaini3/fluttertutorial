18ff756a68fc8b6a63299121fe66b1ae551d6128
Ravi kumar <ravik392320@gmail.com>
2024-03-30 05:09:56 +0530

    day 3 completed

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:installitaion_class_datatype_variable_theme_route/home.dart';
 
@@ -10,7 +12,26 @@ class Loginpage extends StatefulWidget {
 }
 
 class _LoginpageState extends State<Loginpage> {
+  bool checkbutton = false;
   String name = '';
+  var _formkey = GlobalKey<FormState>();
+
+  void gotohome() async {
+    if (_formkey.currentState!.validate()) {
+      setState(() {});
+      checkbutton = true;
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.push(context, MaterialPageRoute(
+        builder: (context) {
+          return const Homepage();
+        },
+      ));
+      setState(() {
+        checkbutton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,63 +66,107 @@ class _LoginpageState extends State<Loginpage> {
       body: Center(
         child: SizedBox(
           width: 500,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 200,
-                width: 200,
-                child: Image.network(
-                    fit: BoxFit.cover,
-                    'https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo='),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Welcome $name',
-                style: const TextStyle(
-                    color: Colors.blue,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextField(
-                autocorrect: true,
-                enabled: true,
-                decoration: const InputDecoration(
-                  suffixIcon: Icon(Icons.email),
-                  suffixIconColor: Colors.blue,
-                  hintText: 'enter your email',
-                  hintStyle: TextStyle(color: Colors.green),
-                  labelText: 'email',
-                  labelStyle: TextStyle(color: Colors.brown),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Image.network(
+                      fit: BoxFit.cover,
+                      'https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo='),
                 ),
-                onChanged: (value) {
-                  name = value;
-                  setState(() {});
-                },
-              ),
-              const TextField(
-                obscureText: true,
-                autocorrect: true,
-                enabled: true,
-                decoration: InputDecoration(
-                  suffixIcon: Icon(Icons.lock),
-                  suffixIconColor: Colors.red,
-                  hintText: 'enter your password',
-                  labelStyle: TextStyle(color: Colors.brown),
-                  hintStyle: TextStyle(color: Colors.red),
-                  labelText: 'password',
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {
+                Text(
+                  'Welcome $name',
+                  style: const TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  autocorrect: true,
+                  enabled: true,
+                  decoration: const InputDecoration(
+                    suffixIcon: Icon(Icons.email),
+                    suffixIconColor: Colors.blue,
+                    hintText: 'enter your email',
+                    hintStyle: TextStyle(color: Colors.green),
+                    labelText: 'email',
+                    labelStyle: TextStyle(color: Colors.brown),
+                  ),
+                  onChanged: (value) {
+                    name = value;
+                    setState(() {});
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "user email can't be empty";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                TextFormField(
+                    obscureText: true,
+                    autocorrect: true,
+                    enabled: true,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.lock),
+                      suffixIconColor: Colors.red,
+                      hintText: 'enter your password',
+                      labelStyle: TextStyle(color: Colors.brown),
+                      hintStyle: TextStyle(color: Colors.red),
+                      labelText: 'password',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "password can't be empty";
+                      } else if (value.length < 6) {
+                        return "passworn must be alteast 6 character";
+                      } else {
+                        return null;
+                      }
+                    }),
+                const SizedBox(
+                  height: 20,
+                ),
+
+                // another way to create animated button
+                InkWell(
+                  onTap: () => gotohome(),
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    height: 50,
+                    width: checkbutton ? 50 : 150,
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(checkbutton ? 50 : 10),
+                        color: Colors.teal),
+                    child: checkbutton
+                        ? Icon(
+                            Icons.done_outline,
+                            color: Colors.white,
+                          )
+                        : Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                    alignment: Alignment.center,
+                  ),
+                )
+                /* ElevatedButton(
+                  onPressed: () async {
+                    setState(() {});
+                    checkbutton = true;
+                    await Future.delayed(Duration(seconds: 1));
                     Navigator.push(context, MaterialPageRoute(
                       builder: (context) {
                         return const Homepage();
@@ -114,14 +179,18 @@ class _LoginpageState extends State<Loginpage> {
                       shadowColor: Colors.brown,
                       foregroundColor: Colors.tealAccent,
                       minimumSize: const Size(180, 40)),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ))
-            ],
+                  child: checkbutton
+                      ? Icon(Icons.done)
+                      : Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                )*/
+              ],
+            ),
           ),
         ),
       ),
